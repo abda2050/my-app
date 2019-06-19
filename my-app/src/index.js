b82847f38ee2a2debcd2ef618a7070e8e2ba55e8
Abdullah Yaser <abda2050@gmail.com>
2019-06-19 13:47:30 +0400

    made the page say hello to the user

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const element = <h1>Hello, World!</h1>
-
-ReactDOM.render(
+function  fromatName(user) {
+    return user.firstName + ' ' + user.lastName;
+};
+const user =  {
+    firstName : 'Abdullah',
+    lastName : 'Yaser'
+}; 
+const element = (
+    <h1>
+        Hello, {fromatName(user)}!
+    </h1>
+);
+ReactDOM.render (
     element,
     document.getElementById('root')
-  );
-  
+);
