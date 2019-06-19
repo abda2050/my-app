9a9d91a8d84a26f1ec51357276af4179b3210d12
Abdullah Yaser <abda2050@gmail.com>
2019-06-19 09:19:27 +0400

    putted the message in a constant

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const element = <h1>Hello, World!</h1>
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    element,
     document.getElementById('root')
   );
   
